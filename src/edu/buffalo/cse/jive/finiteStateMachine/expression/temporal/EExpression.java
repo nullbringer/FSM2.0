043cd478689249c560280f71333776231cb4cbd4
Shashank Raghunath <shashank.raghunath15@gmail.com>
2019-02-22 13:37:07 -0500

    Implemented evaluate of EExpression

--- src/edu/buffalo/cse/jive/finiteStateMachine/expression/temporal/EExpression.java
+++ src/edu/buffalo/cse/jive/finiteStateMachine/expression/temporal/EExpression.java
@@ -1,8 +1,14 @@
 package edu.buffalo.cse.jive.finiteStateMachine.expression.temporal;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import edu.buffalo.cse.jive.finiteStateMachine.expression.expression.Expression;
 import edu.buffalo.cse.jive.finiteStateMachine.expression.expression.UnaryExpression;
 import edu.buffalo.cse.jive.finiteStateMachine.models.Context;
+import edu.buffalo.cse.jive.finiteStateMachine.models.State;
+import edu.buffalo.cse.jive.finiteStateMachine.util.Pair;
 
 /**
  * @author Shashank Raghunath
@@ -10,6 +16,8 @@ import edu.buffalo.cse.jive.finiteStateMachine.models.Context;
  *
  */
 public class EExpression extends UnaryExpression<Expression> {
+
+	private boolean result;
 
 	public EExpression() {
 		super();
@@ -21,6 +29,19 @@ public class EExpression extends UnaryExpression<Expression> {
 
 	@Override
 	public Boolean evaluate(Context context) {
-		return null;
+		result = false;
+		evaluate(null, context.getCurrentState(), new HashSet<Pair<State, State>>(), getExpression(),
+				context.getStates());
+		return result;
+	}
+
+	private State evaluate(State prev, State curr, Set<Pair<State, State>> visited, Expression expression,
+			Map<State, Set<State>> states) {
+		for (State next : states.get(curr)) {
+			if (visited.add(new Pair<State, State>(curr, next)) && !result)
+				result = expression.evaluate(
+						new Context(curr, evaluate(curr, next, visited, expression, states), states)) || result;
+		}
+		return curr;
 	}
 }
