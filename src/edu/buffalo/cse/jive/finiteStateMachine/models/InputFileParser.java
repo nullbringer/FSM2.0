3270e6f7e5cd15088de55f6e71761102d102f08c
urbangeek <gupta.amlan@gmail.com>
2019-06-25 15:52:29 -0400

    remove unnecessary call stack

--- src/edu/buffalo/cse/jive/finiteStateMachine/models/InputFileParser.java
+++ src/edu/buffalo/cse/jive/finiteStateMachine/models/InputFileParser.java
@@ -14,6 +14,8 @@ import java.util.concurrent.LinkedBlockingQueue;
  * @author Shashank Raghunath
  * @email sraghuna@buffalo.edu
  *
+ *@author Amlan Gupta
+ *@email amlangup@buffalo.edu
  */
 /**
  * Parser the Input CSV file and extracts all attributes and Field Write Events
@@ -41,33 +43,20 @@ public class InputFileParser {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
 			String line;
 			int counter = 0;
-			HashMap<String,Stack<String>> callStacks = new HashMap<String,Stack<String>>();
 			String lastMethod = null;
 			
 			while ((line = bufferedReader.readLine()) != null) {
 				
 				// Logic for method consolidation
 				if (line.contains("Method Call")) {
-					String thrd = line.substring(0, line.indexOf(',')).replace("\"","").trim();
 					lastMethod = line.substring(line.indexOf("target=")+7);
 					lastMethod = lastMethod.substring(lastMethod.indexOf('#')+1).replace("\"","").trim();
 					lastMethod = lastMethod + ":" + counter++;
-					if (callStacks.containsKey(thrd)) {
-						Stack<String> threadStack = callStacks.get(thrd);
-						threadStack.push(lastMethod);
-						callStacks.put(thrd, threadStack);
-					}
-					else {
-						Stack<String> threadStack = new Stack<String>();
-						threadStack.push(lastMethod);
-						callStacks.put(thrd,threadStack);
-						//System.out.println("lastMethod =" + lastMethod);
-					}
+					
 				}
 				if (line.contains("Field Write")) {
 					String[] tokens = line.split(",");
 					
-					String thread = tokens[0].replace("\"","").trim();
 					String object = tokens[4].substring(tokens[4].indexOf("=") + 1).replace("\"", "").trim();
 					String field = tokens[5].substring(0, tokens[5].indexOf("=")).replace("\"", "").trim();
 					String value = tokens[5].substring(tokens[5].indexOf("=") + 1).replace("\"", "").trim();
@@ -76,23 +65,10 @@ public class InputFileParser {
 					
 					allFields.add(fld);
 				}
-				if (line.contains("Method Returned")) {
-					String thrd = line.substring(0,line.indexOf(','));
-					Stack<String> threadStack = callStacks.get(thrd);
-					callStacks.put(thrd, threadStack);
-				}
-				
-				
-				
-//				if (line.contains("Field Write")) {
-//					String[] tokens = line.split(",");
-//					String object = tokens[4].substring(tokens[4].indexOf("=") + 1).replace("\"", "").trim();
-//					String field = tokens[5].substring(0, tokens[5].indexOf("=")).replace("\"", "").trim();
-//					String value = tokens[5].substring(tokens[5].indexOf("=") + 1).replace("\"", "").trim();
-//					String fld = object.replace("/", ".") + "." + field;
-//					events.add(new Event(fld, value));
-//					allFields.add(fld);
+//				if (line.contains("Method Returned")) {
+//					//operations for this method ended
 //				}
+				
 			}
 			bufferedReader.close();
 		} catch (IOException e) {
