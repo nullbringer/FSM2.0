9eafc49d5ee4fcc7603481ebaf1c82c28414994a
urbangeek <gupta.amlan@gmail.com>
2019-06-25 15:02:45 -0400

    Update OfflineMonitor.java

--- src/edu/buffalo/cse/jive/finiteStateMachine/monitor/OfflineMonitor.java
+++ src/edu/buffalo/cse/jive/finiteStateMachine/monitor/OfflineMonitor.java
@@ -11,14 +11,16 @@ import edu.buffalo.cse.jive.finiteStateMachine.models.Event;
  */
 public class OfflineMonitor extends Monitor {
 
-	public OfflineMonitor(Set<String> keyFields, BlockingQueue<Event> source) {
-		super(keyFields, source);
+	public OfflineMonitor(Set<String> keyFields, BlockingQueue<Event> source, boolean shouldConsolidateByMethod) {
+		super(keyFields, source, shouldConsolidateByMethod);
 	}
 
 	@Override
 	public void run() {
 		for (Event event : getSource()) {
-			buildStates(event);
+			generateEventMap(event);
 		}
+		
+		buildStates();
 	}
 }
